a076234b4377d1e45bf367f94ccea4a716e6ee54
Arnau Canela <arnau.canela22@gmail.com>
2024-03-26 17:47:05 +0100

    second commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,20 +1,27 @@
 import { Inter, Shanti, Raleway, Poppins } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
-export const raleway = Raleway({subsets: ['latin'], weight: ['400', '700']})
-export const shanti = Shanti({ subsets: ["latin"], weight: ['400'] });
-export const poppins = Poppins({ subsets: ["latin"], weight: ['400', '500', '600'] });
+export const raleway = Raleway({ subsets: ["latin"], weight: ["400", "700"] });
+export const shanti = Shanti({ subsets: ["latin"], weight: ["400"] });
+export const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600"],
+});
 
 export const metadata = {
-  title: "Angely",
+  title: "Skailee",
   description: "Business digitalitzation agency",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-black overflow-hidden`}>{children}</body>
+      <body className={`${inter.className} bg-black overflow-hidden`}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
