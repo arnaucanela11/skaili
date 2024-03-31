49a0e86d04f2b9881d53f50301a8dd81eeb75384
Arnau Canela <arnau.canela22@gmail.com>
2024-03-31 23:11:34 +0200

    commit sixteen

--- src/app/components/Questions.js
+++ src/app/components/Questions.js
@@ -173,7 +173,7 @@ function Questions() {
         <div className="bg-[#ffffffa0] border-[#ffffffa0] h-[0.5px] w-[100%]" />
       </div>
       <div
-        className="question__div__1"
+        className="question__div"
         onClick={() =>
           setQuestion((prev) => {
             if (prev == "5") {
@@ -205,6 +205,49 @@ function Questions() {
               We will try to make the process as easy and simple as possible for
               you, but we might need you to provide specific media (such as
               photos or videos) in order to achieve the desired result.
+            </span>
+          </div>
+        ) : (
+          ""
+        )}
+        <div className="bg-[#ffffffa0] border-[#ffffffa0] h-[0.5px] w-[100%]" />
+      </div>
+      <div
+        className="question__div__1"
+        onClick={() =>
+          setQuestion((prev) => {
+            if (prev == "5") {
+              return "0";
+            } else return "5";
+          })
+        }
+      >
+        <div className="question__not__hidden__div">
+          <span className={`question__main__span `}>
+            What happens if I ultimately don't like the result?
+          </span>
+          <svg
+            width="32"
+            height="32"
+            viewBox="0 0 32 32"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M27.75 16C27.75 16.1989 27.671 16.3897 27.5303 16.5303C27.3897 16.671 27.1989 16.75 27 16.75H16.75V27C16.75 27.1989 16.671 27.3897 16.5303 27.5303C16.3897 27.671 16.1989 27.75 16 27.75C15.8011 27.75 15.6103 27.671 15.4697 27.5303C15.329 27.3897 15.25 27.1989 15.25 27V16.75H5C4.80109 16.75 4.61032 16.671 4.46967 16.5303C4.32902 16.3897 4.25 16.1989 4.25 16C4.25 15.8011 4.32902 15.6103 4.46967 15.4697C4.61032 15.329 4.80109 15.25 5 15.25H15.25V5C15.25 4.80109 15.329 4.61032 15.4697 4.46967C15.6103 4.32902 15.8011 4.25 16 4.25C16.1989 4.25 16.3897 4.32902 16.5303 4.46967C16.671 4.61032 16.75 4.80109 16.75 5V15.25H27C27.1989 15.25 27.3897 15.329 27.5303 15.4697C27.671 15.6103 27.75 15.8011 27.75 16Z"
+              fill="white"
+            />
+          </svg>
+        </div>
+        {question == "5" ? (
+          <div className="ml-5 mb-5 mt-2 w-[90%]">
+            <span className={`question__1__text ${poppins.className}`}>
+              The idea is for us to receive some feedback from you during the
+              digitization process. However, if you are ultimately not satisfied
+              with the delivered product, we can either make some final
+              adjustments, or if the issue is significant, we can cancel the
+              contract. This way, the website wouldn't be delivered, and no
+              payment would be required.
             </span>
           </div>
         ) : (
--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -2,9 +2,10 @@
 import Image from "next/image";
 import AngelyLogo from "../assets/SkailiLogo.png";
 import Link from "next/link";
-import BgShapes from "../assets/BgShapesContact.png";
-import ContactCrown from '../assets/ContactCrown.png'
-import BgShapesSmallDevices from '../assets/BgShapesContactSmallDevices.png'
+import BgShapes from "../assets/ContactBg.png";
+import ContactCrown from "../assets/ContactCrown.png";
+import ContactImage from "../assets/ContactImage.png";
+import BgShapesSmallDevices from "../assets/BgShapesContactSmallDevices.png";
 import "./contact.css";
 import { ErrorMessage, Field, Formik } from "formik";
 import * as yup from "yup";
@@ -58,6 +59,7 @@ function page() {
   };
   return (
     <main className="w-100vw h-[100vh] overflow-x-hidden overflow-y-scroll relative">
+      <Image className="contact__bg" src={BgShapes} />
       <section className="header__section">
         <Link href={"/"}>
           <Image alt="logo" src={AngelyLogo} className="Skaili__logo" />
@@ -66,184 +68,189 @@ function page() {
           Home page
         </Link>
       </section>
-      <section className="main__contact">
-        <div className="contact__form__div">
-          <Formik
-            validationSchema={loginSchema}
-            initialValues={initailaState}
-            onSubmit={(values, submitProps) =>
-              handleSubmitContact(values, submitProps)
-            }
-          >
-            {({
-              values,
-              handleSubmit,
-              handleBlur,
-              handleChange,
-              setFieldValue,
-            }) => (
-              <form onSubmit={handleSubmit}>
-                <div className="form__div__flex">
-                  <div className="contact__data__div">
-                    <span className="contact__data__span">Contact Data</span>
-                    <div className="signup__form">
-                      <div className="signup__form__1">
-                        <label
-                          htmlFor="email"
-                          className="field__label__flex"
-                        >
-                          <label className="field__label">Name*</label>
-                          <Field
-                            name="name"
-                            type="text"
-                            className="name__field"
-                            placeholder="e.g. Jose"
-                          />
-                          <ErrorMessage
-                            name="name"
-                            render={(msg) => (
-                              <span style={{ color: "white" }}>
-                                {msg.charAt(0).toUpperCase() + msg.slice(1)}
-                              </span>
-                            )}
-                          />
-                        </label>
-                        <label
-                          htmlFor="company"
-                          className="field__label__flex"
-                        >
-                          <label className="field__label">
-                            Company (optional)
-                          </label>
-                          <Field
-                            name="company"
-                            type="text"
-                            className="name__field"
-                            placeholder="e.g. Jose’s barbershop"
-                          />
-                          <ErrorMessage
-                            name="company"
-                            render={(msg) => (
-                              <span style={{ color: "white" }}>
-                                {msg.charAt(0).toUpperCase() + msg.slice(1)}
-                              </span>
-                            )}
-                          />
-                        </label>
-                      </div>
-                      <label
-                        htmlFor="email"
-                        className="field__label__flex"
-                      >
-                        <label className="field__label">Email*</label>
-                        <Field
-                          name="email"
-                          type="text"
-                          className="name__field__1"
-                          placeholder="e.g. jose.miranda@gmail.com"
-                        />
-                        <ErrorMessage
-                          name="email"
-                          render={(msg) => (
-                            <span style={{ color: "white" }}>
-                              {msg.charAt(0).toUpperCase() + msg.slice(1)}
-                            </span>
-                          )}
-                        />
-                      </label>
-                    </div>
-                    <div className="tell__us__div">
-                      <div className="tell__us__div__1">
-                        <span className="contact__data__span">
-                          Tell us about your idea
-                        </span>
-                        <span className="contact__data__span__1">
-                          (in any language)
-                        </span>
-                      </div>
-                      <Field
-                        as="textarea"
-                        name="message"
-                        className="message__field"
-                        placeholder="I want to create..."
-                      />
-                      <ErrorMessage
-                        name="message"
-                        render={(msg) => (
-                          <span style={{ color: "white" }}>
-                            {msg.charAt(0).toUpperCase() + msg.slice(1)}
-                          </span>
-                        )}
-                      />
-                    </div>
-                    <div className="privacy__rights">
-                      <label className="privacy__rights__label">
-                        <div class="checkbox-wrapper-46">
-                          <Field
-                            name="acceptPolicy"
-                            type="checkbox"
-                            id="cbx-46"
-                            class="inp-cbx"
-                          />
-                          <label for="cbx-46" class="cbx">
-                            <span>
-                              <svg
-                                viewBox="0 0 12 10"
-                                height="10px"
-                                width="12px"
-                              >
-                                <polyline points="1.5 6 4.5 9 10.5 1"></polyline>
-                              </svg>
-                            </span>
-                          </label>
-                        </div>
-                        <span className="privacy__rights__span">
-                          I have read and accept the{" "}
-                          <Link href={"/data_protection"} className="underline">
-                            data protection policy{" "}
-                          </Link>
-                        </span>
-                      </label>
-                      <ErrorMessage
-                        name="acceptPolicy"
-                        className="mt-3"
-                        render={(msg) => (
-                          <span style={{ color: "white" }}>
-                            {msg.charAt(0).toUpperCase() + msg.slice(1)}
-                          </span>
-                        )}
-                      />
-                    </div>
-                    <button type="submit" className="button__submit">
-                      <span className="send__span">Send</span>
-                      <svg
-                        width="12"
-                        height="9"
-                        viewBox="0 0 12 9"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          d="M11.2674 4.85355C11.4626 4.65829 11.4626 4.34171 11.2674 4.14645L8.08539 0.964466C7.89013 0.769204 7.57355 0.769204 7.37828 0.964466C7.18302 1.15973 7.18302 1.47631 7.37828 1.67157L10.2067 4.5L7.37828 7.32843C7.18302 7.52369 7.18302 7.84027 7.37828 8.03553C7.57355 8.2308 7.89013 8.2308 8.08539 8.03553L11.2674 4.85355ZM0.913818 5H10.9138V4H0.913818V5Z"
-                          fill="black"
-                        />
-                      </svg>
-                    </button>
-                  </div>
-                </div>
-              </form>
-            )}
-          </Formik>
-          <Image className="contact__crown" src={ContactCrown}></Image>
-          <Image className="contact__crown__1" src={ContactCrown}></Image>
-          <Image className="bg__shapes__small__devices" src={BgShapesSmallDevices}></Image>
+      <div className="main__flex__contact">
+        <div className="title__div">
+          <h1 className="main__contact__title">GET IN TOUCH</h1>
+          <h3 className="main__contact__subtitle">
+            Ask for a quote or any questions, we will respond to you as soon as
+            possible.
+          </h3>
         </div>
-        <Image
+        <section className="main__contact">
+          <div className="main__contact__shadow__1" />
+          <div className="main__contact__shadow__2" />
+          <div className="contact__form__div">
+            <Formik
+              validationSchema={loginSchema}
+              initialValues={initailaState}
+              onSubmit={(values, submitProps) =>
+                handleSubmitContact(values, submitProps)
+              }
+            >
+              {({
+                values,
+                handleSubmit,
+                handleBlur,
+                handleChange,
+                setFieldValue,
+              }) => (
+                <form className="contact__form__div" onSubmit={handleSubmit}>
+                  <h2 className="contact__data__span">
+                    Provide below your data and explain your idea:
+                  </h2>
+                  <div className="name__fields__div">
+                    <label htmlFor="email" className="field__label__flex__1">
+                      {/* <label className="field__label">Name*</label> */}
+                      <Field
+                        name="name"
+                        type="text"
+                        className="name__field"
+                        placeholder="Name*"
+                      />
+                      <ErrorMessage
+                        name="name"
+                        render={(msg) => (
+                          <span
+                            className="error__message"
+                            style={{ color: "white" }}
+                          >
+                            {msg.charAt(0).toUpperCase() + msg.slice(1)}
+                          </span>
+                        )}
+                      />
+                    </label>
+                    <label htmlFor="company" className="field__label__flex__1">
+                      {/* <label className="field__label">Company (optional)</label> */}
+                      <Field
+                        name="company"
+                        type="text"
+                        className="name__field"
+                        placeholder="Company name"
+                      />
+                      <ErrorMessage
+                        name="company"
+                        render={(msg) => (
+                          <span
+                            className="error__message"
+                            style={{ color: "white" }}
+                          >
+                            {msg.charAt(0).toUpperCase() + msg.slice(1)}
+                          </span>
+                        )}
+                      />
+                    </label>
+                  </div>
+                  <label htmlFor="email" className="field__label__flex">
+                    {/* <label className="field__label">Email*</label> */}
+                    <Field
+                      name="email"
+                      type="text"
+                      className="name__field__1"
+                      placeholder="Email*"
+                    />
+                    <ErrorMessage
+                      name="email"
+                      render={(msg) => (
+                        <span
+                          className="error__message"
+                          style={{ color: "white" }}
+                        >
+                          {msg.charAt(0).toUpperCase() + msg.slice(1)}
+                        </span>
+                      )}
+                    />
+                  </label>
+                  <label htmlFor="message" className="field__label__flex">
+                    <Field
+                      as="textarea"
+                      name="message"
+                      className="message__field"
+                      placeholder="I want to create..."
+                    />
+                    <ErrorMessage
+                      name="message"
+                      render={(msg) => (
+                        <span
+                          className="error__message"
+                          style={{ color: "white" }}
+                        >
+                          {msg.charAt(0).toUpperCase() + msg.slice(1)}
+                        </span>
+                      )}
+                    />
+                  </label>
+                  <div className="privacy__rights">
+                    <label className="privacy__rights__label">
+                      <div class="checkbox-wrapper-46">
+                        <Field
+                          name="acceptPolicy"
+                          type="checkbox"
+                          id="cbx-46"
+                          class="inp-cbx"
+                        />
+                        <label for="cbx-46" class="cbx">
+                          <span>
+                            <svg viewBox="0 0 12 10" height="10px" width="12px">
+                              <polyline points="1.5 6 4.5 9 10.5 1"></polyline>
+                            </svg>
+                          </span>
+                        </label>
+                      </div>
+                      <span className="privacy__rights__span">
+                        I have read and accept the{" "}
+                        <Link href={"/data_protection"} className="underline">
+                          data protection policy{" "}
+                        </Link>
+                      </span>
+                    </label>
+                    <ErrorMessage
+                      name="acceptPolicy"
+                      className="mt-3"
+                      render={(msg) => (
+                        <span
+                          className="error__message"
+                          style={{ color: "white" }}
+                        >
+                          {msg.charAt(0).toUpperCase() + msg.slice(1)}
+                        </span>
+                      )}
+                    />
+                  </div>
+                  <button type="submit" className="button__submit">
+                    <span className="send__span">Send</span>
+                    <svg
+                      width="12"
+                      height="9"
+                      viewBox="0 0 12 9"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M11.2674 4.85355C11.4626 4.65829 11.4626 4.34171 11.2674 4.14645L8.08539 0.964466C7.89013 0.769204 7.57355 0.769204 7.37828 0.964466C7.18302 1.15973 7.18302 1.47631 7.37828 1.67157L10.2067 4.5L7.37828 7.32843C7.18302 7.52369 7.18302 7.84027 7.37828 8.03553C7.57355 8.2308 7.89013 8.2308 8.08539 8.03553L11.2674 4.85355ZM0.913818 5H10.9138V4H0.913818V5Z"
+                        fill="black"
+                      />
+                    </svg>
+                  </button>
+                </form>
+              )}
+            </Formik>
+            <Image className="contact__crown" src={ContactCrown}></Image>
+            <Image className="contact__crown__1" src={ContactCrown}></Image>
+            <Image
+              className="bg__shapes__small__devices"
+              src={BgShapesSmallDevices}
+            ></Image>
+          </div>
+          {/* <Image
             alt="bg__shapes"
             className="bg__shapes__contact"
             src={BgShapes}
-          ></Image>
-      </section>
+          ></Image> */}
+          <div className="contact__picture__div">
+            <Image src={ContactImage} className="contact__image"></Image>
+          </div>
+        </section>
+      </div>
     </main>
   );
 }
